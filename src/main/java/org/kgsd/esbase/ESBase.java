1c1ce32a641f0093d87568923c6be1672e2af4db
Guna Sekhar Dora <gunasekhar.dora3@gmail.com>
2020-03-17 10:47:00 +0530

    Initial commit

---
+++ src/main/java/org/kgsd/esbase/ESBase.java
@@ -0,0 +1,9 @@
+package org.kgsd.esbase;
+
+public class ESBase {
+    public static void main(String[] args) {
+        System.out.println("\n-----------------------------------------");
+        System.out.println("\n               ESBASE                    ");
+        System.out.println("\n-----------------------------------------");
+    }
+}

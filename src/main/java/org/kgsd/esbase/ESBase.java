ee59177fca72396dc7567a9bf3fb94cce6379c7b
Guna Sekhar Dora <gunasekhar.dora3@gmail.com>
2020-03-19 09:41:11 +0530

    feat: add log4j2

--- src/main/java/org/kgsd/esbase/ESBase.java
+++ src/main/java/org/kgsd/esbase/ESBase.java
@@ -3,6 +3,7 @@ package org.kgsd.esbase;
 import com.google.inject.Guice;
 import com.google.inject.Inject;
 import com.google.inject.Injector;
+import lombok.extern.log4j.Log4j2;
 import ro.pippo.controller.Controller;
 import ro.pippo.controller.ControllerApplication;
 import ro.pippo.core.Pippo;
@@ -11,6 +12,7 @@ import java.util.Set;
 
 import static org.kgsd.esbase.common.Constants.APP_PORT;
 
+@Log4j2
 public class ESBase extends ControllerApplication { 
     @Inject
     public ESBase(Set<Controller> controllers) {
@@ -27,8 +29,8 @@ public class ESBase extends ControllerApplication {
             ESBase esBase = injector.getInstance(ESBase.class);
             (new Pippo(esBase)).start(APP_PORT);
         } catch (Exception e) {
-            //TODO: Add log
-            System.out.println("Error in initializing ESBase, exiting... " + e);
+            log.error("Error in initializing ESBase App, exiting...", e);
+            System.out.println("Error in initializing ESBase App, exiting... " + e);
             System.exit(1);
         }
     }
